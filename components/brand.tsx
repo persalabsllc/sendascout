1b974e61a97484a46e142ee2061eac214ed85505
persalabsllc <persalabsllc@gmail.com>
2026-08-28 12:08:27 -0400

    Polish optimized brand mark

--- components/brand.tsx
+++ components/brand.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export function Brand({ href = "/" }: { href?: string }) {
   return (
     <Link className="brand" href={href} aria-label="Send a Scout home">
-      <img className="brand-mark" src="/logo-mark.svg" alt="" />
+      <Image className="brand-mark" src="/logo-mark.svg" alt="" width={43} height={43} priority />
       <span className="brand-name"><span>Send a</span> Scout</span>
     </Link>
   );
